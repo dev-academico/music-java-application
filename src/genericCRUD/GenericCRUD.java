a6368573b61212090cdc40c9f63764fe557cf985
Your Name <you@example.com>
2025-12-05 09:31:13 -0300

    Generic CRUD

---
+++ src/genericCRUD/GenericCRUD.java
@@ -0,0 +1,49 @@
+package genericCRUD;
+
+import AVLTree.AvlTree;
+import AVLTree.Node;
+import Interfaces.InterfaceAvlTree;
+
+public class GenericCRUD<T extends InterfaceAvlTree> {
+    AvlTree<T> tree;
+
+    public GenericCRUD(){
+        this.tree = new AvlTree<T>();
+    }
+
+    public T AddItem(T item) {
+        tree.insert(item);
+
+        return item;
+    }
+
+    public T DeleteItem(T item) {
+        tree.delete(item.getKey());
+
+        return item;
+    }
+
+    public T FindItem(int key) {
+        Node<T> node = tree.search(tree.root, key);
+
+        T item = node.value;
+
+        return item;
+    }
+
+    public T UpdateItem(int key) {
+        T itemToBeUpdated = FindItem(key);
+
+        itemToBeUpdated.update();
+
+        return itemToBeUpdated;
+    }
+
+    public void ShowItem(T item) {
+        System.out.println(item);
+    }
+
+    public void ShowAllItems() {
+
+    }
+}
